de078334234def1a2c55989c61d94fa03f4dbcc7
Kathymch <kathemart0427@gmail.com>
2020-04-22 22:18:33 -0600

    Cambios Katherine

---
+++ src/routes/proveedor.js
@@ -0,0 +1,104 @@
+const {
+    Router
+} = require('express');
+const router = Router();
+
+const Proveedor = require('../models/Proveedor');
+const {
+    isAuthenticated
+} = require('../helpers/auth');
+
+router.get('/proveedor', isAuthenticated, async (req, res) => {
+    const proveedor = await Proveedor.find().sort([
+        ['updatedAt', 'descending']
+    ]);
+
+    res.render('proveedor/new-proveedor', {
+        proveedor   
+    });
+});
+
+router.post('/proveedor/new-proveedor', isAuthenticated, async (req, res) => {
+    const {
+        CodigoProveedor,
+        
+    } = req.body;
+    const CodigoProveedor= req.body.selectCMT;
+    const UnidadDeMedida= req.body.selectUM;
+    const CodigoProducto = 1;
+    const errors = [];
+    //Chequeo si está vacío el textbox
+    
+    if (!Descripcion) {
+        errors.push({
+            text: "Escribe Descripcion"
+        });
+    }
+    if (!PuntosReOrden) {
+        errors.push({
+            text: "Escribe Puntos ReOrden."
+        });
+    }
+
+    if (errors.length > 0) {
+        res.render("productos/new-producto", {
+            errors, //No se quita
+            CodigoMateriaPrima,
+            Descripcion,
+            PuntosReOrden,
+            UnidadDeMedida,
+            CodigoProducto
+        });
+    } else {
+        const cantidadProductos = await Producto.find().countDocuments();
+
+        const newProducto = new Producto({
+            CodigoMateriaPrima,
+            Descripcion,
+            PuntosReOrden,
+            UnidadDeMedida
+        });
+        newProducto.CodigoProducto = cantidadProductos + 1;
+        await newProducto.save();
+        req.flash("success_msg", "Producto Añadido");
+        res.redirect("/productos");
+    }
+});
+
+router.get('/productos/edit/:id', isAuthenticated, async (req, res) => {
+    const producto = await Producto.findById(req.params.id);
+
+    res.render("productos/edit-producto", {
+        producto
+    });
+});
+
+router.put('/productos/edit-producto/:id', isAuthenticated, async (req, res) => {
+    const {
+        //CodigoMateriaPrima,
+        Descripcion,
+        PuntosReOrden
+        //UnidadDeMedida
+    } = req.body;
+    const CodigoMateriaPrima= req.body.selectCMT;
+    const UnidadDeMedida= req.body.selectUM;
+    await Producto.findByIdAndUpdate(req.params.id, {
+        CodigoMateriaPrima,
+        Descripcion,
+        PuntosReOrden,
+        UnidadDeMedida
+    });
+    req.flash("success_msg", "Producto Editado Exitosamente");
+    res.redirect("/productos");
+});
+
+router.delete('/productos/delete/:id', isAuthenticated, async (req, res) => {
+    await Producto.findByIdAndDelete(req.params.id);
+    req.flash("success_msg", "Producto Eliminado Exitosamente");
+    res.redirect("/productos");
+});
+
+
+
+
+module.exports = router;
